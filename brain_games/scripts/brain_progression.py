abc8702145e7769cb38fa034b011c36838360445
Konstantin Chupin <chupin.k@yandex.ru>
2022-06-05 10:41:52 +0500

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -6,6 +6,10 @@ from random import randint, choice
 
 
 def progression_games():
+    """Shows the player a series of numbers forming an arithmetic progression by
+    replacing any of the numbers with two dots. The player must determine this
+    number."""
+
     print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
@@ -17,20 +21,20 @@ def progression_games():
         diff = randint(1, 10)
         list_t = []
         a = 0
-        elem = 0
+        el = 0  # secret element by default
         while a < 10:
             a += 1
             num_1 += diff
             list_t.append(num_1)
-            elem = choice(list_t)
-        list_t[list_t.index(elem)] = '..'
+            el = choice(list_t)  # random selection of a secret element
+        list_t[list_t.index(el)] = '..'
         question = ' '.join(map(str, list_t))
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
-        if int(answer) == elem:
+        if int(answer) == el:
             print('Correct!')
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{elem}'")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{el}'.")
             print(f"Let's try again, {name}")
             break
         attempts -= 1
