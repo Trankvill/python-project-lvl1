5d643d5b26e3d8061ecce960b0082a644ce38dc9
Konstantin Chupin <chupin.k@yandex.ru>
2022-06-11 21:42:22 +0500

    update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,6 +3,9 @@
 import prompt
 
 
+NUMBER_OF_ROUNDS = 3
+
+
 def launch_game(game):
     """Greets the player and checks the player's response to the task
     in the running game. Controls the overall logic of the game."""
@@ -11,24 +14,19 @@ def launch_game(game):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
-    print(game.GAME_CONDITION)
+    print(game.GAME_RULE)
 
-    attempts = 3
-
-    while attempts > 0:
-        correct_answer, question = game.get_game()
+    for i in range(NUMBER_OF_ROUNDS):
+        correct_answer, question = game.get_round()
         print(f'Question: {question}')
 
         answer = prompt.string('Your answer: ')
 
         if answer == correct_answer:
             print('Correct!')
-        else:
-            print(f"'{answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            break
-        attempts -= 1
+        elif answer != correct_answer:
+            return print(f"""'{answer}' is wrong answer ;(. \
+Correct answer was '{correct_answer}'.
+Let's try again, {name}!""")
 
-        if attempts == 0:
-            print(f'Congratulations, {name}!')
+    print(f'Congratulations, {name}!')
